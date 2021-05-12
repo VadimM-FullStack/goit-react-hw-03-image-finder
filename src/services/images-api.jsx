9cce9ff967df62f7bf885b30491a13e12e6058ee
Vadim Matsko <mvadim3101@gmail.com>
2021-05-12 11:35:46 +0300

    Add defaults to axios

--- src/services/images-api.jsx
+++ src/services/images-api.jsx
@@ -2,18 +2,29 @@ import axios from "axios";
 import { Component } from "react";
 import PropTypes from "prop-types";
 
+const BASE_URL = "https://pixabay.com/api/";
+const API_KEY = "20677562-9c517eaf9134a4d9aa45dfdde";
+
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: "photo",
+  orientation: "horizontal",
+  per_page: 12,
+};
+
 class ImagesApi extends Component {
   static propTypes = {
     searchQuery: PropTypes.string.isRequired,
     currentPage: PropTypes.number.isRequired,
   };
 
-  fetchImages = ({ searchQuery, currentPage }) => {
-    return axios
-      .get(
-        `https://pixabay.com/api/?q=${searchQuery}&page=${currentPage}&key=20677562-9c517eaf9134a4d9aa45dfdde&image_type=photo&orientation=horizontal&per_page=12`,
-      )
-      .then(responce => responce.data.hits);
+  fetchImages = async ({ searchQuery, currentPage }) => {
+    const { data } = await axios.get("", {
+      params: { searchQuery, currentPage },
+    });
+    return data.hits;
   };
 }
+
 export default ImagesApi;
